6d7f01984755bc6767a52da350e959bf00f57d9b
Adrian Dunham <adunham95@gmail.com>
2021-07-19 16:53:38 -0400

    Added banner and header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react'
+import { Banner } from './Banner';
+
+const links = [
+  {title: "Home", link:'/'},
+  {title: "Blog", link:'/blog'},
+  {title: "Projects", link:'/project'},
+]
+
+const banner = {
+  show: false,
+  text: "We announced a brand new product!", 
+  cta: "Learn more", 
+  link: "#",
+}
+
+export const Header = () => {
+  const [navOpen, setNavOpen] = useState(false)
+  return (
+    <>
+      {
+        banner.show && <Banner {...banner}/>
+      }
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* <!-- Mobile menu button--> */}
+              <button type="button"
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                aria-controls="mobile-menu"
+                onClick={()=>setNavOpen(!navOpen)}
+                aria-expanded="false">
+                <span className="sr-only">Open main menu</span>
+                {/* <!--
+              Icon when menu is closed.
+  
+              Heroicon name: outline/menu
+  
+              Menu open: "hidden", Menu closed: "block"
+            --> */}
+                <svg className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true">
+                  <path strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                {/* <!--
+              Icon when menu is open.
+  
+              Heroicon name: outline/x
+  
+              Menu open: "block", Menu closed: "hidden"
+            --> */}
+                <svg className="hidden h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true">
+                  <path strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex-shrink-0 flex items-center text-white">
+            Adrian Dunham
+              </div>
+              <div className="hidden sm:block sm:ml-6">
+                <div className="flex space-x-4" />
+              </div>
+            </div>
+            <div className="hidden sm:block sm:ml-6 absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              {
+                links.map(l =>
+                  <a href={l.link}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">{l.title}</a>,
+                )
+              }
+            </div>
+          </div>
+        </div>
+  
+        {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+        <div className={`sm:hidden ${navOpen? "block": 'hidden'}`}
+          id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            {
+              links.map(l =>
+                <a href={l.link}
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{l.title}</a>,
+              )
+            }
+            
+          </div>
+        </div>
+      </nav>
+    </>
+  )}
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import config from "../../data/SiteConfig";
+import { Header } from "../components/Header/Header";
 import "./index.css";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="layout-container">
+    <div>
       <Helmet>
-        <meta name="description" content={config.siteDescription} />
+        <meta name="description"
+          content={config.siteDescription} />
         <html lang="en" />
       </Helmet>
-      {children}
+      <Header/>
+      <main>
+        {children}
+      </main>
     </div>
   );
 }
