c03d400b5063870470068762092b5a71965b5460
Adrian Dunham <adunham95@gmail.com>
2021-07-19 19:29:00 -0400

    Removed unused query

--- src/templates/landing.jsx
+++ src/templates/landing.jsx
@@ -22,27 +22,3 @@ function Landing({ data }) {
 }
 
 export default Landing;
-
-/* eslint no-undef: "off" */
-export const pageQuery = graphql`
-  query LandingQuery {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          fields {
-            slug
-            date
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            tags
-            cover
-            date
-          }
-        }
-      }
-    }
-  }
-`;
