6dacfb6c64a353d787520de23ab51679e22f1f64
Adrian Dunham <adunham95@gmail.com>
2021-07-19 15:59:12 -0400

    Removed unused component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../layout";
-import About from "../components/About/About";
 import config from "../../data/SiteConfig";
 
 function AboutPage() {
