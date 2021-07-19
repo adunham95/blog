61e9cfec4ec4ef67a85fd8a4c05072e9310437d2
Adrian Dunham <adunham95@gmail.com>
2021-07-19 17:07:28 -0400

    UPdated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        28: '7rem',
+        'screen-1/3': 'calc(100vh / 3)',
+        'screen-1/2': 'calc(100vh / 2)',
+        'screen-1/4': 'calc(100vh / 4)',
+        'screen-3/4': 'calc(100vh * 0.75)',
+      },
+    },
   },
   variants: {
     extend: {},
