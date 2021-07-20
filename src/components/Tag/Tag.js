ddc0882e13dfc7a169a73acdd36206d4fa708ed6
Adrian Dunham <adunham95@gmail.com>
2021-07-20 17:02:16 -0400

    Added tag text color selector

--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -3,6 +3,15 @@ import { graphql, Link, StaticQuery } from 'gatsby'
 import React from 'react'
 import _ from "lodash";
 
+function pickTextColorBasedOnBgColorSimple(bgColor, lightColor, darkColor) {
+  const color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  const r = parseInt(color.substring(0, 2), 16); // hexToR
+  const g = parseInt(color.substring(2, 4), 16); // hexToG
+  const b = parseInt(color.substring(4, 6), 16); // hexToB
+  return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186)
+    ? darkColor : lightColor;
+}
+
 export const Tag = ({name, className, to='#'}) => (
   <StaticQuery
     query={graphql`
@@ -28,7 +37,7 @@ export const Tag = ({name, className, to='#'}) => (
         const tagData = info[0];
         return(
           <Link to={`/tags/${_.kebabCase(name)}`}
-            className={`${defaultStyles} text-white bg-black`}
+            className={`${defaultStyles} ${pickTextColorBasedOnBgColorSimple(tagData.color, 'text-white', 'text-black')}`}
             style={{backgroundColor:tagData.color}}>{name}</Link>
         )
       }
