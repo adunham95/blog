c218fde2ba105662e469628baba9dcf4677eb385
Adrian Dunham <adunham95@gmail.com>
2021-07-20 08:01:24 -0400

    Added create new project

---
+++ newProject.js
@@ -0,0 +1,81 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const moment = require("moment");
+const someObject = require('./data/tags.json');
+
+const questions = [
+  {
+    type : "input",
+    name : `name`,
+    message : "Project Name: ",
+    default: "coding",
+  },
+  {
+    type : "input",
+    name : `description`,
+    message : "Project Description: ",
+    default: '',
+  },
+  {
+    type : "list",
+    name : `category`,
+    message : "Project Category: ",
+    choices: ['Personal', 'Work'],
+  },
+  {
+    name: "isOpenSource",
+    type: "confirm",
+    message: "Is this project open source?",
+  },
+  {
+    type : "checkbox",
+    name : `tags`,
+    message : "Tags: ",
+    choices: someObject,
+  },
+];
+
+function buildPost(answers){
+  console.log(answers);
+  
+  const fileName = `${new Date().toISOString()}.json`;
+  
+  const fileData = 
+  {
+    title: answers.name,
+    tags: answers.tags,
+    openSource: answers.isOpenSource,
+    description: answers.description,
+
+  }
+  console.log(fileData)
+
+  const dir = 'content/project'
+
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+        
+    fs.writeFile(`${dir}/${fileName}`, JSON.stringify(fileData), (err) => {
+      if (err) throw err;
+      console.log('File is created successfully.');
+    });
+  }
+}
+
+function startPompt(){
+  inquirer
+    .prompt(questions)
+    .then(answers => {
+      buildPost(answers);
+    })
+    .catch(error => {
+      if(error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+        console.log("error")
+      }
+    });
+}
+
+startPompt()
