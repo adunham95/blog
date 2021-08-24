dcd5a73013fa1332b531a5fbe4bf5c3cce705eac
adunham95 <adunham95@gmail.com>
2021-08-24 10:54:01 -0400

    Added icons

---
+++ src/components/Icons/VueIcon.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function VueIcon(props) {
+  return <svg role="img" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" height={32} width={32} {...props}><title>{"Vue.js"}</title><path d="M24,1.61H14.06L12,5.16,9.94,1.61H0L12,22.39ZM12,14.08,5.16,2.23H9.59L12,6.41l2.41-4.18h4.43Z" /></svg>;
+}
+
+export default VueIcon;
